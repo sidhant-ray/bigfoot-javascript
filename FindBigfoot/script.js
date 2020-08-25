db74a5f0bc09190761ec8e0ceaf8101dedaf85a1
Sidhant Ray <ray.sidhant007@gmail.com>
2020-08-26 04:24:40 +0530

    Add files via upload

---
+++ FindBigfoot/script.js
@@ -0,0 +1,13 @@
+function loadBigFoot() {
+  document.getElementById('bigFoot').src = 'bigfoot.png'
+}
+function moveBigFoot() {
+    alert('Woohoo, you win! You found Bigfoot!')
+  var picture = document.getElementById('bigFoot')
+
+  var x = Math.random() * 300
+  var y = Math.random() * 300
+
+  picture.style.top = x + 'px'
+  picture.style.left = y + 'px'
+  }
